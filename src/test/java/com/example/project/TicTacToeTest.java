6a2f77358255370aae538862adefc1fcd1ddca7a
ertrldmrhn <148891981+ertrldmrhn@users.noreply.github.com>
2025-04-26 16:45:55 +0000

    fix: add missing import for assertThrows

--- src/test/java/com/example/project/TicTacToeTest.java
+++ src/test/java/com/example/project/TicTacToeTest.java
@@ -11,6 +11,7 @@
  package com.example.project;
 
  import static org.junit.jupiter.api.Assertions.assertEquals;
+ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.beans.Transient;
 
